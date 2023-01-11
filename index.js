369b028b488a306cdfc006a09bf58a064e3517ac
“Konstantinos <kostaks@outlook.com>
2023-01-11 16:31:23 +0100

    iteration 2 done

--- index.js
+++ index.js
@@ -5,3 +5,26 @@
 
 
 // Iteration 3: Loops
+const nameCapital = driver.toUpperCase()
+let updatedName = ``
+for (let i=0 ; i < nameCapital.length ; i++){
+ updatedName += (nameCapital[i] + ` `)
+}
+console.log (updatedName)
+
+let reverseOrder = ``
+for (let y=navigator.length - 1 ; y >= 0 ; y--){
+  const char = navigator[y]
+  reverseOrder += char
+}
+console.log (reverseOrder)
+
+if (driver.localeCompare(navigator) === -1) {
+    console.log(`The driver's name goes first.`)
+    }
+else if (driver.localeCompare(navigator) === 1) {
+    console.log(`Yo, the navigator goes first definitely.`)
+    }
+else {
+    console.log(`What?! You both have the same name?`)
+    }
